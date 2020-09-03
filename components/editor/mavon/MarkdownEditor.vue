e618109118714e9548bb46ea89379d05b320b1fa
virskor <virskor>
2020-09-03 14:31:16 +0800

    编辑器层级Bug

--- components/editor/mavon/MarkdownEditor.vue
+++ components/editor/mavon/MarkdownEditor.vue
@@ -84,7 +84,7 @@ export default {
 				imagelink: false, // 图片链接
 				code: true, // code
 				table: true, // 表格
-				fullscreen: true, // 全屏编辑
+				fullscreen: false, // 全屏编辑
 				readmodel: false, // 沉浸式阅读
 				htmlcode: true, // 展示html源码
 				help: true, // 帮助
